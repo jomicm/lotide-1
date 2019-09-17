0a66095397066a8846f03e33743dd0794f13c6aa
Jenny Nguy <j.nguy809@gmail.com>
2019-09-17 22:12:18 +0000

    function removes items in one array from another array

---
+++ without.js
@@ -0,0 +1,43 @@
+let eqArrays = function(array1, array2) {
+  let truthy;
+  for (let i = 0; i < (array1.length + array2.length); i++) {
+    if (array1[i] !== array2[i]) {
+      truthy = false;
+    }
+  } if (truthy === undefined) {
+    truthy = true;
+  }
+  return truthy;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  let truthy;
+  for (let i = 0; i < (array1.length + array2.length); i++) {
+    if (array1[i] !== array2[i]) {
+      truthy = false;
+    }
+  } if (truthy === undefined) {
+    truthy = true;
+  }
+  if (truthy) {
+    console.log(`✅Arrays are the same: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑Arrays are NOT the same: ${array1} !== ${array2}`);
+  }
+}
+
+const without = function(source, itemsToRemove) {
+  let withoutItems = [];
+  for (let i = 0; i < source.length; i++) {
+    if (itemsToRemove.includes(source[i]) === false) {
+      withoutItems.push(source[i]);
+    }
+  } return withoutItems;
+}
+
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
+const words = ["hello", "world", "lighthouse"];
+console.log(without(["hello", "world", "lighthouse"], ["lighthouse"])); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
