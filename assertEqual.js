b0be14b1415a4e0c171e0361863e0e04e6900d77
Jenny Nguy <j.nguy809@gmail.com>
2019-09-17 20:55:39 +0000

    added message to function

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+const assertEqual = function(actual, expected, message) {
+  if (actual === expected) { 
+    console.log(`✅✅✅ Assertion Passed`);
   } else {
-    console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed`);
   }
-};
+}
 
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual("Hello", "Hello");
