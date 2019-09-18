d96ba12000f90cd381802e692a36a5523d931210
Jenny Nguy <j.nguy809@gmail.com>
2019-09-18 16:31:10 +0000

    added printed function for given test

--- countOnly.js
+++ countOnly.js
@@ -39,6 +39,7 @@ const firstNames = [
 
 const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
 
+console.log(countOnly(firstNames, result1))
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
