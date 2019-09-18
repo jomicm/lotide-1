1170c7969b075d1c9ffc979e2c4fed2bf2e4a910
Jenny Nguy <j.nguy809@gmail.com>
2019-09-18 18:40:35 +0000

    linted code

--- letterPositions.js
+++ letterPositions.js
@@ -8,7 +8,7 @@ const eqArrays = function(array1, array2) {
     truthy = true;
   }
   return truthy;
-}
+};
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
@@ -16,22 +16,22 @@ const assertArraysEqual = function(array1, array2) {
   } else {
     console.log(`🛑Assertion Failed: ${array1} !== ${array1}`);
   }
-}
+};
 
 const letterPositions = function(sentence) {
   let count = 0; // push count number in place of index number when it is not the first occurance of the letter
   const results = {};
-    for (letter of sentence) { // loops through each letter in sentence
-      if (letter !== " ") { // will not count for spaces
-        const resultsCount = [] // pushes letter and index/count to an object before adding to array
-        if (results[letter]) { // if the letter is already accounted for, push the count nummber instead of the index number
-          results[letter].push(count)
-        } else {
-          resultsCount.push(sentence.indexOf(letter)) // if letter is not accounted for, push the index number (indexOf finds only the first index)
-          results[letter] = resultsCount // creates the key-value pair in the results object
-        } 
-      } count += 1; // increase count by 1 for each time the loop is run, such that count = index
-    }
+  for (const letter of sentence) { // loops through each letter in sentence
+    if (letter !== " ") { // will not count for spaces
+      const resultsCount = []; // pushes letter and index/count to an object before adding to array
+      if (results[letter]) { // if the letter is already accounted for, push the count nummber instead of the index number
+        results[letter].push(count);
+      } else {
+        resultsCount.push(sentence.indexOf(letter)); // if letter is not accounted for, push the index number (indexOf finds only the first index)
+        results[letter] = resultsCount; // creates the key-value pair in the results object
+      }
+    } count += 1; // increase count by 1 for each time the loop is run, such that count = index
+  }
   return results;
 };
 
@@ -43,7 +43,7 @@ assertArraysEqual(letterPositions("lighthouse in the house").e, [9, 16, 22]);
 assertArraysEqual(letterPositions("lighthouse in the house").h, [3, 5, 15, 18]);
 
 // expected output for "lighthouse in the house"
-// { 
+// {
 //   l: [0],
 //   i: [1, 11],
 //   g: [2],
