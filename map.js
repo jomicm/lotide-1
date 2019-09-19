983e662f1200144b80ea4c6c0506fa962c6c4892
Jenny Nguy <j.nguy809@gmail.com>
2019-09-19 17:27:13 +0000

    map function takes an array and desired change

---
+++ map.js
@@ -0,0 +1,38 @@
+const eqArrays = function(array1, array2) {
+  let truthy;
+  for (let i = 0; i < (array1.length + array2.length); i++) {
+    if (array1[i] !== array2[i]) {
+      truthy = false;
+    }
+  } if (truthy === undefined) {
+    truthy = true;
+  }
+  return truthy;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑Assertion Failed: ${array1} !== ${array1}`);
+  }
+};
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+// TESTING MAP FUNCTION:
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
+assertArraysEqual(results1[0], ['g']);
+assertArraysEqual(results1[4], ['t']);
