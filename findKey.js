2bfc7a21f0a6a2d0527bb4abdfcb3624c7e48e7d
Jenny Nguy <j.nguy809@gmail.com>
2019-09-19 18:44:08 +0000

    function to return value for the desired key

---
+++ findKey.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected, message) {
+  if (actual === expected) { 
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+const findKey = function(object, callback) {
+  for (let item in object) {
+    if (callback(object[item])) {
+      return item
+    }
+  }
+}
+
+console.log(assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), 'noma')) // => "noma"
