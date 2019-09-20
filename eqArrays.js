4c184c975495d4a4e09731c5d36857fef2cfb0c4
Jenny Nguy <j.nguy809@gmail.com>
2019-09-20 21:57:35 +0000

    linted code

--- eqArrays.js
+++ eqArrays.js
@@ -22,14 +22,14 @@ let eqArrays = function(array1, array2) {
     }
   }
   return true;
-}
+};
 
 assertEqual(eqArrays([1, 2, 3, 4, 5], [1, 2, 3, 4]), false);
 assertEqual(eqArrays(["1", "2", 3], ["1", "2", "3"]), false);
 assertEqual(eqArrays(["one", "two"], ["one", "two"]), true);
 assertEqual(eqArrays(["one", "two"], [1, "two"]), false);
 
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true) // => true
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
 
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false) // => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false) // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
