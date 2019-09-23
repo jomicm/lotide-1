40960a2633831a24966f0c6e7bca11727aee3ad2
Jenny Nguy <j.nguy809@gmail.com>
2019-09-23 17:42:04 +0000

    use mocha for headTest middleTest tailTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,19 @@
 const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual')
+const assert = require('chai').assert;
 
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
+// console.log(middle([1, 2, 3, 4])); // => [2, 3]
+// console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+// console.log(middle([1, 2, 3])) // => [2]
+// console.log(middle([1, 2, 3, 4, 5])) // => [3]
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 6]), [3]);
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+// assertArraysEqual(middle([1, 2, 3]), [2]);
+// assertArraysEqual(middle([1, 2, 3, 4, 6]), [3]);
+
+describe('#middle', () => {
+  it('should return [2, 3] for [1, 2, 3, 4]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,13 @@
 const assertEqual = require('../assertEqual');
 const tail = require('../tail')
+const assert = require('chai').assert
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+// tail(words);
+// assertEqual(words.length, 3);
+
+describe('tail', () => {
+  it('should return 3 for words.length', () => {
+    assert.strictEqual(tail(words).length, 2);
+  });
+});
