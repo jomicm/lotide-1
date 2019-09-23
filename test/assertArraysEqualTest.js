1de40bfe7e5ef3949aa525e15b27cb4d75bec2a7
jomicm <singularity.jmcm@gmail.com>
2019-09-23 14:17:45 -0400

    Clear comments

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,11 +1,6 @@
 const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual');
 
-// assertArraysEqual([1, 2, 3, 4, 5], [1, 2, 3, 4]);
-// assertArraysEqual(["1", "2", 3], ["1", "2", "3"]);
-// assertArraysEqual(["one", "two"], ["one", "two"]);
-// assertArraysEqual(["one", "two"], [1, "two"]);
-
 describe('#assertArrayEqual', () => {
   it('should return false for [1, 2, 3, 4, 5] and [1, 2, 3, 4]', () => {
     assert.strictEqual(assertArraysEqual([1, 2, 3, 4, 5], [1, 2, 3, 4]), false);
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -2,18 +2,6 @@ const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
 const assert = require('chai').assert;
 
-
-
-// assertEqual(eqArrays([1, 2, 3, 4, 5], [1, 2, 3, 4]), false);
-// assertEqual(eqArrays(["1", "2", 3], ["1", "2", "3"]), false);
-// assertEqual(eqArrays(["one", "two"], ["one", "two"]), true);
-// assertEqual(eqArrays(["one", "two"], [1, "two"]), false);
-
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
-
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
-
 describe('#eqArray', () => {
   it('should return false for [1,2,3,4,5] and [1,2,3,4]',()=>{
     assert.strictEqual(eqArrays([1,2,3,4,5], [1,2,3,4]), false);
